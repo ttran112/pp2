e19d87cefd22326c8ec6e6d10912c03539d567c3
ttran112 <ttran112@mail.greenriver.edu>
2021-01-13 23:45:48 -0500

    Thanh Version

---
+++ PairProgram2.php
@@ -0,0 +1,15 @@
+<?php
+//Author: Thanh Tran
+//Date:
+//Url:
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>Hello World</h1>
+</body>
+</html>
