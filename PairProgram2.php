749d7e3d19c94955c1821e9a0c532dd1c276474a
ttran112 <ttran112@mail.greenriver.edu>
2021-01-17 19:45:35 -0500

    just add

--- PairProgram2.php
+++ PairProgram2.php
@@ -1,15 +1,26 @@
 <?php
 //Author: Thanh Tran
-//Date:
-//Url:
+//Date: jan 15, 2021
+//Url:index.php
+//Project Title: Pair Program 2
+
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Pair Program 2</title>
 </head>
 <body>
-<h1>Hello World</h1>
+<?php
+//print out something
+echo "<p>PHP Array Practice</p>";
+
+
+?>
 </body>
 </html>
