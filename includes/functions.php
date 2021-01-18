fd498d5f477c0961eda2e85eb1bbfc029b13f1db
ttran112 <ttran112@mail.greenriver.edu>
2021-01-17 20:12:28 -0500

    include file

---
+++ includes/functions.php
@@ -0,0 +1,18 @@
+<?php
+//Author: Thanh Tran
+//Date: jan 15, 2021
+//Url:index.php
+//Project Title: Pair Program 2
+
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+function printArray($number)
+{
+    foreach ( $number as $item) {
+        echo "$item <br>";
+    }
+}
+//printArray($number);
+
